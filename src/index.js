0514a2df6816de38a3a66c39c1c0e2b0d80661a5
Your Name <you@example.com>
2022-08-14 18:57:41 +0400

    feat: latest stable version of index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  num = String(Math.abs(n));
+  let arr = [];
+  for (let i = 0; i < num.length; i++) {
+    if (i < num.length-1) {arr[num.length-1-i]=num.slice(i,i+1)}
+    else {arr[0] = num.slice(i)}
+  }
+  let res = Number(arr.join(''));
+  return(res);
 }
